19f38c491bc999725d42b4a812226dc7468e9e80
Dillon Craw <68667382+crawnic311@users.noreply.github.com>
2021-12-26 12:24:54 -0700

    Header Styled and Implemented

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,15 @@
 import React from 'react'
+import styles from './Home.module.css'
 
 const Home = () => {
-  return <div>This is the home page.</div>
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <div className={styles.grid}>{/*<RecipeList />*/}</div>
+        <div className={styles.container}>{/*} <RecipeNav />*/}</div>
+      </main>
+    </div>
+  )
 }
 
 export default Home
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,19 +1,65 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import styles from './Navbar.module.css'
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false)
+  const openMenu = () => {
+    setIsOpen(!isOpen)
+  }
   return (
-    <ul>
-      <li>
-        <Link to="/">Home</Link>
-      </li>
-      <li>
-        <Link to="/Create">Create</Link>
-      </li>
-      <li>
-        <Link to="/about">About</Link>
-      </li>
-    </ul>
+    <>
+      <header className={styles.header}>
+        <nav className={styles.navbar}>
+          <Link to="/">
+            <span className={styles.navlogo}>
+              <img
+                src="/foody-logo.png"
+                alt="logo"
+                width="150"
+                height="69"
+                className={styles.foodylogo}
+              />
+            </span>
+          </Link>
+          <ul
+            className={
+              isOpen === false
+                ? styles.navmenu
+                : styles.navmenu + ' ' + styles.active
+            }
+          >
+            <li className={styles.navitem}>
+              <Link to="/">
+                <span className={styles.navlink}>Home</span>
+              </Link>
+            </li>
+            <li className={styles.navitem}>
+              <Link to="/create">
+                <span className={styles.navlink}>Create</span>
+              </Link>
+            </li>
+            <li className={styles.navitem}>
+              <Link to="/about">
+                <span className={styles.navlink}>About</span>
+              </Link>
+            </li>
+          </ul>
+          <button
+            className={
+              isOpen === false
+                ? styles.hamburger
+                : styles.hamburger + ' ' + styles.active
+            }
+            onClick={openMenu}
+          >
+            <span className={styles.bar}></span>
+            <span className={styles.bar}></span>
+            <span className={styles.bar}></span>
+          </button>
+        </nav>
+      </header>
+    </>
   )
 }
 
