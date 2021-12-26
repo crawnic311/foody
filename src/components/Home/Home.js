0a05c2f4fb112e13dbede50f23c9413789e1e1ac
Dillon Craw <68667382+crawnic311@users.noreply.github.com>
2021-12-26 12:30:26 -0700

    RecipeNav and RecipeList Modules Reimplemented

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import styles from './Home.module.css'
+import RecipeNav from './Recipes/RecipeNav'
+import RecipeList from './Recipes/RecipeList'
 
 const Home = () => {
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-        <div className={styles.grid}>{/*<RecipeList />*/}</div>
-        <div className={styles.container}>{/*} <RecipeNav />*/}</div>
+        <div className={styles.grid}>
+          <RecipeList />
+        </div>
+        <div className={styles.container}>
+          {' '}
+          <RecipeNav />
+        </div>
       </main>
     </div>
   )
