1916fa7045a1ba0d72eb8095f08b1962e80455e7
Dillon Craw <68667382+crawnic311@users.noreply.github.com>
2021-12-26 12:50:28 -0700

    Login form without Error message Reimplemented

--- src/App.js
+++ src/App.js
@@ -1,21 +1,59 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Home from './components/Home/Home'
 import About from './components/About./About'
 import Create from './components/Create/Create'
 import Navbar from './components/Navbar/Navbar'
+import LoginForm from './components/Login/LoginForm'
 import { Routes, Route } from 'react-router-dom'
 import './App.css'
 
 function App() {
+  const adminUser = {
+    name: '',
+    email: 'dillon.craw@gmail.com',
+    password: '123',
+  }
+
+  const [user, setUser] = useState({ name: '', email: '', password: '' })
+  const [error, setError] = useState('')
+
+  const Login = (details) => {
+    setUser({
+      name: details.name,
+      email: details.email,
+      password: details.password,
+    })
+    console.log(details)
+
+    if (
+      details.email === adminUser.email &&
+      details.password === adminUser.password
+    ) {
+      console.log('Logged in')
+    } else {
+      console.log('Details do not match!')
+      setError('Details do not match!')
+    }
+  }
+
+  const Logout = () => {
+    console.log('Logout')
+  }
   return (
-    <div className="App">
-      <Navbar />
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route exact path="/create" element={<Create />} />
-        <Route exact path="/about" element={<About />} />
-      </Routes>
-    </div>
+    <>
+      {user.email !== '' ? (
+        <div className="App">
+          <Navbar />
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/create" element={<Create />} />
+            <Route exact path="/about" element={<About />} />
+          </Routes>
+        </div>
+      ) : (
+        <LoginForm Login={Login} error={error} />
+      )}
+    </>
   )
 }
 
---
+++ src/components/Login/LoginForm.js
@@ -0,0 +1,65 @@
+import React, { useState } from 'react'
+import styles from './LoginForm.module.css'
+
+const LoginForm = (props) => {
+  const Login = props.Login
+  const error = props.error
+
+  const [details, setDetails] = useState({ name: '', email: '', password: '' })
+
+  const submitHandler = (e) => {
+    e.preventDefault()
+
+    Login(details)
+  }
+
+  return (
+    <div className={styles.LoginWrapper}>
+      <form className={styles.LoginForm} onSubmit={submitHandler}>
+        <div className={styles.ErrorWrapper}>
+          {error !== '' ? <p className={styles.Error}>{error}</p> : ''}
+        </div>
+        <div className={styles.formgroup}>
+          <input
+            type="text"
+            placeholder="Name"
+            name="name"
+            id={styles.name}
+            onChange={(e) => setDetails({ ...details, name: e.target.value })}
+            value={details.name}
+          />
+        </div>
+        <div className={styles.formgroup}>
+          <input
+            type="text"
+            placeholder="Email"
+            name="email"
+            id={styles.email}
+            onChange={(e) => setDetails({ ...details, email: e.target.value })}
+            value={details.email}
+          />
+        </div>
+        <div className={styles.formgroup}>
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            id={styles.password}
+            onChange={(e) =>
+              setDetails({ ...details, password: e.target.value })
+            }
+            value={details.password}
+          />
+          <span className={styles.ForgotPassword}>Forgot Password?</span>
+        </div>
+        <div className={styles.formgroup} id={styles.loginButton}>
+          <input type="submit" value="LOGIN" id={styles.login} />
+          <span href="" className={styles.NewHere}>
+            New here? Create an account.
+          </span>
+        </div>
+      </form>
+    </div>
+  )
+}
+export default LoginForm
