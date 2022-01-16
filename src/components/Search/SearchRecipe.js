184d4d5367f7b629f59a9c9d7bd9082c67e3c986
Dillon Craw <68667382+crawnic311@users.noreply.github.com>
2022-01-15 18:42:17 -0700

    Minor Changes

--- src/components/Search/SearchRecipe.js
+++ src/components/Search/SearchRecipe.js
@@ -13,7 +13,12 @@ const SearchRecipe = ({
   return (
     <form className={styles.SearchRecipe} onSubmit={(e) => e.preventDefault()}>
       <label htmlFor="search" className={styles.searchLabel}></label>
-      <input id="search" type="text" role="searchbox" />
+      <input
+        id="search"
+        type="text"
+        role="searchbox"
+        placeholder={placehoder}
+      />
     </form>
   )
 }
