4cc78f9912e2c0de8236c301862edadb13d7555a
Dillon Craw <68667382+crawnic311@users.noreply.github.com>
2022-02-10 15:12:44 -0700

    Next Button Working Properly

--- src/components/Home/Recipes/RecipeList.js
+++ src/components/Home/Recipes/RecipeList.js
@@ -11,11 +11,11 @@ const RecipeList = ({ recipeID, setRecipeID, recipes, handleDelete }) => {
     id: 0,
     title: 'display test',
     image: '',
-    description: '',
-    servings: '',
-    prepTime: '',
-    cookTime: '',
-    instructions: ['', ''],
+    description: 'display test',
+    servings: '99',
+    prepTime: '99',
+    cookTime: '99',
+    instructions: ['Inst', 'Ruct'],
   })
 
   return (
@@ -24,58 +24,55 @@ const RecipeList = ({ recipeID, setRecipeID, recipes, handleDelete }) => {
         <div className={styles.Recipe}>
           <div className={styles.RecipeTextHolder}>
             <div className={styles.RecipeTitle} id="title">
-              {
-                //recipes[recipeID].title
-                displayRecipe.title
-              }
+              {displayRecipe.title}
             </div>
             <div className={styles.TimeHolder}>
               <span className={styles.rTime}>
-                Servings: {recipes[recipeID].servings}
+                Servings: {displayRecipe.servings}
               </span>
               <span className={styles.rTime}>
-                Prep Time: {recipes[recipeID].preptime}
+                Prep Time: {displayRecipe.prepTime}
               </span>
               <span className={styles.rTime}>
-                Cook Time: {recipes[recipeID].cooktime}
+                Cook Time: {displayRecipe.cookTime}
               </span>
             </div>
             <div className={styles.RecipeDescription}>
               <span className={styles.Description}>Description:</span>
               <p className={styles.RecipeDetails}>
-                {recipes[recipeID].description}
+                {displayRecipe.description}
               </p>
             </div>
             <div className={styles.RecipeInstructions}>
               <span className={styles.Instructions}>Instructions:</span>
               <p className={styles.InstructionsFor}>
-                {recipes[recipeID].instructions.length >= 1
-                  ? `For the ` + recipes[recipeID].instructions[0] + `: `
+                {displayRecipe.instructions.length >= 1
+                  ? `For the ` + displayRecipe.instructions[0] + `: `
                   : 'No instructions added'}
               </p>
               <p className={styles.InstructionsP}>
-                {recipes[recipeID].instructions.length >= 2
-                  ? recipes[recipeID].instructions[1]
+                {displayRecipe.instructions.length >= 2
+                  ? displayRecipe.instructions[1]
                   : ''}
               </p>
               <p className={styles.InstructionsFor}>
-                {recipes[recipeID].instructions.length >= 3
-                  ? `For the ` + recipes[recipeID].instructions[2] + `: `
+                {displayRecipe.instructions.length >= 3
+                  ? `For the ` + displayRecipe.instructions[2] + `: `
                   : ''}
               </p>
               <p className={styles.InstructionsP}>
-                {recipes[recipeID].instructions.length >= 4
-                  ? recipes[recipeID].instructions[3]
+                {displayRecipe.instructions.length >= 4
+                  ? displayRecipe.instructions[3]
                   : ''}
               </p>
               <p className={styles.InstructionsFor}>
-                {recipes[recipeID].instructions.length >= 5
-                  ? `For the ` + recipes[recipeID].instructions[4] + `: `
+                {displayRecipe.instructions.length >= 5
+                  ? `For the ` + displayRecipe.instructions[4] + `: `
                   : ''}
               </p>
               <p className={styles.InstructionsP}>
-                {recipes[recipeID].instructions.length >= 6
-                  ? recipes[recipeID].instructions[5]
+                {displayRecipe.instructions.length >= 6
+                  ? displayRecipe.instructions[5]
                   : ''}
               </p>
             </div>
@@ -117,9 +114,10 @@ const RecipeList = ({ recipeID, setRecipeID, recipes, handleDelete }) => {
             id={styles.recipeForward}
             onClick={() => {
               if (recipes.length > 1)
-                console.log('displayRecipe', displayRecipe)
+                console.log('recipesLength Greater than 1')
+              console.log('displayRecipe', displayRecipe)
               //setRecipeID(recipeID + 1)
-              for (let i = 0; recipes.length < i; i++) {
+              for (let i = 0; recipes.length > i; i++) {
                 if (recipes[i].id > displayRecipe.id) {
                   setDisplayRecipe(recipes[i])
                   console.log('displayRecipe', displayRecipe)
