35bb821176b64dbe95582469534e96e91f5840f4
Dillon Craw <68667382+crawnic311@users.noreply.github.com>
2022-02-16 14:31:13 -0700

    More Cloudinary Strucutre Built

--- src/cloudinary/Cloud.js
+++ src/cloudinary/Cloud.js
@@ -1,8 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
 
 const Cloud = () => {
+  const [image, setImage] = useState('')
+
   const uploadImage = (files) => {
-    console.log(files[0])
+    const formData = new FormData()
+    formData.append('file', files[0])
+    formData.append('upload_preset', 'kgvi7e5c')
+
+    axios
+      .post('https://api.cloudinary.com/v1/doybhneia/image/upload', formData)
+      .then((response) => {
+        console.log(response)
+      })
   }
 
   return (
@@ -10,9 +21,10 @@ const Cloud = () => {
       <input
         type="file"
         onChange={(e) => {
-          uploadImage(e.target.files)
+          setImage(e.target.files[0])
         }}
       />
+      <button onClick={uploadImage}>Upload Image to Cloudinary</button>
     </div>
   )
 }
