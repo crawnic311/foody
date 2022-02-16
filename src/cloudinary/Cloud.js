57401f99de21d3b7264add6ed24b5c65a94daca3
Dillon Craw <68667382+crawnic311@users.noreply.github.com>
2022-02-16 15:04:43 -0700

    Setting up for Cloudinary

--- src/cloudinary/Cloud.js
+++ src/cloudinary/Cloud.js
@@ -2,29 +2,38 @@ import React, { useState } from 'react'
 import axios from 'axios'
 
 const Cloud = () => {
-  const [image, setImage] = useState('')
+const [imageSrc, setImageSrc] = useState()
 
-  const uploadImage = (files) => {
-    const formData = new FormData()
-    formData.append('file', files[0])
-    formData.append('upload_preset', 'kgvi7e5c')
+const handleOnChange = (changeEvent) => {
+  const reader = new FileReader()
 
-    axios
-      .post('https://api.cloudinary.com/v1/doybhneia/image/upload', formData)
-      .then((response) => {
-        console.log(response)
-      })
+  reader.onload = function(onLoadEvent) {
+    setImageSrc(onLoadEvent.target.result)
+    setUploadData(undefined)
   }
+
+  reader.readAsDataURL(changeEvent.target.files[0])
+}
 
   return (
     <div>
-      <input
-        type="file"
-        onChange={(e) => {
-          setImage(e.target.files[0])
-        }}
-      />
-      <button onClick={uploadImage}>Upload Image to Cloudinary</button>
+      <form method="post" onChange={handleOnChange} onSubmit={handleOnSubmit}>
+        <p>
+          <input type="file" name="file" />
+        </p> 
+
+        <img src={imageSrc} />
+
+        {imageSrc && !uploadData && (
+          <p>
+            <button>Upload Files</button>
+          </p>
+        )}
+
+        {uploadData && (
+          <code><pre>{JSON.stringify(uploadData, null, 20}</pre></code>
+        )}
+      </form>
     </div>
   )
 }
