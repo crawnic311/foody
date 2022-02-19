8fda8b199ce165ccb8895d093e2fe8081a72c6f4
Dillon Craw <68667382+crawnic311@users.noreply.github.com>
2022-02-18 19:02:56 -0700

    Secrets and key added

--- src/cloudinary/Cloud.js
+++ src/cloudinary/Cloud.js
@@ -1,5 +1,7 @@
 export async function getStaticProps() {
-  const results = await fetch (`https://api.cloudinary.com/v1_1/{process.env.CLOUDINARY_CLOUD_NAME}/resources/image`, headers: {
+  const results = await fetch (`https://api.cloudinary.com/v1_1/${process.env.CLOUDINARY_CLOUD_NAME}/resources/image`, 
+    headers: {
+      Authorization: `Basic ${Buffer.from(process.env.CLOUDINARY_API_KEY + ':' + process.env.CLOUDINARY_API_SECRET).toString('base64')}`
 
   })
   return (
