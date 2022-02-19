bdd11907afa34ee29ec16ca61fd3db687dab3e3e
Dillon Craw <68667382+crawnic311@users.noreply.github.com>
2022-02-19 10:45:21 -0700

    Building gallary

--- src/cloudinary/Cloud.js
+++ src/cloudinary/Cloud.js
@@ -2,8 +2,8 @@ export async function getStaticProps() {
   const results = await fetch (`https://api.cloudinary.com/v1_1/${process.env.CLOUDINARY_CLOUD_NAME}/resources/image`, 
     headers: {
       Authorization: `Basic ${Buffer.from(process.env.CLOUDINARY_API_KEY + ':' + process.env.CLOUDINARY_API_SECRET).toString('base64')}`
-
-  })
+  }).then(r => r.json())
+  console.log('results', results)
   return (
     props: {
 
