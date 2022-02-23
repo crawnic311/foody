cf71fa69fc037472bbfcd04163a00487957d52a8
Dillon Craw <68667382+crawnic311@users.noreply.github.com>
2022-02-22 20:55:08 -0700

    Changes today

--- server/index.js
+++ server/index.js
@@ -17,6 +17,8 @@ app.use(express.json())
 app.use(cors())
 app.use(json())
 
+const { parsed: config } = dotenv
+
 //Routes (app.get, post, put, delete)
 //app.post('/api/register/:id', controller.addUser)
 app.post('/api/register', controller.addUser)
@@ -28,5 +30,20 @@ app.get('/api/recipes', controller.getRecipe)
 app.patch('/api/recipes/:id', controller.setImage)
 
 app.delete('/api/recipes/:id', controller.deleteRecipe)
+
+const BASE_URL = `https://api.cloudinary.com/v1_1/${config.CLOUD_NAME}/resources/image`
+
+const auth = {
+  username: config.API_KEY,
+  password: config.API_SECRET,
+}
+
+app.get(`/photos`, async (req, res) => {
+  const response = await axios.get(BASE_URL, {
+    auth,
+  })
+
+  return res.send(response.data)
+})
 
 app.listen(PORT, () => console.log(`Running on ${PORT}`))
