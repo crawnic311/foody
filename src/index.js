7aae783aea2c6812dd9e8bb9097dd01e3a037e52
Dillon Craw <68667382+crawnic311@users.noreply.github.com>
2022-03-17 10:41:29 -0600

    Toastify and Bootstrap imported to index

--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'react-toastify/dist/react-toastify.cjs.development'
+import { ToastContainer } from 'react-toastify'
 
 ReactDOM.render(
   <BrowserRouter>
     <App />
+    <ToastContainer />
   </BrowserRouter>,
   document.getElementById('root')
 )
