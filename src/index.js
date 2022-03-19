0b9518d394675aeb4a5c66fd787c182fb4d82454
Dillon Craw <68667382+crawnic311@users.noreply.github.com>
2022-03-19 12:16:30 -0600

    Bootstrap uninstalled

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
-import 'bootstrap/dist/css/bootstrap.css'
 //trouble with the react toastify import....
 import 'react-toastify/dist/react-toastify.cjs.development'
 import { ToastContainer } from 'react-toastify'
