12471f7209c7fd5f8a65755e667c27fa3f727743
Dillon Craw <68667382+crawnic311@users.noreply.github.com>
2022-03-21 23:31:40 -0600

    Componenets moved out of about

--- src/components/About./About.js
+++ src/components/About./About.js
@@ -1,16 +1,12 @@
 import React from 'react'
 import Built from './Built/built'
 import styles from './about.module.css'
-import ImageList from '../ImagesList'
-import AddImage from '../AddImage'
 
 const About = ({ imageSrc, setImageSrc, uploadData, setUploadData }) => {
   return (
     <div>
       <p className={styles.built}>Built using: </p>
       <Built />
-      <ImageList />
-      <AddImage />
     </div>
   )
 }
