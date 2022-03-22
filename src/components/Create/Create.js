8fd1efb7059a81cd248c9dd9dd078b47c5139f42
Dillon Craw <68667382+crawnic311@users.noreply.github.com>
2022-03-21 23:31:17 -0600

    Components moved to change from about

--- src/components/Create/Create.js
+++ src/components/Create/Create.js
@@ -2,6 +2,8 @@ import React from 'react'
 import styles from './Create.module.css'
 import { useRef } from 'react'
 import { FaPlus } from 'react-icons/fa'
+import ImageList from '../ImagesList'
+import AddImage from '../AddImage'
 
 const Create = ({
   newRecipe,
@@ -18,97 +20,103 @@ const Create = ({
   const inputRef = useRef()
 
   return (
-    <form
-      method="post"
-      className={styles.createRecipe}
-      onSubmit={(e) => {
-        handleSubmit(e)
-      }}
-    >
-      <label htmlFor="createRecipe">Create Recipe</label>
-      <input
-        autoFocus
-        ref={inputRef}
-        id={styles.titleInput}
-        placeholder="Recipe Title"
-        value={newRecipe.title}
-        required
-        onChange={(e) => setNewRecipe({ ...newRecipe, title: e.target.value })}
-      ></input>
-      <input
-        id={styles.descriptionInput}
-        placeholder="Recipe Description"
-        value={newRecipe.description}
-        required
-        onChange={(e) =>
-          setNewRecipe({ ...newRecipe, description: e.target.value })
-        }
-      ></input>
-      <input
-        id={styles.titleInput}
-        placeholder="Servings"
-        value={newRecipe.servings}
-        onChange={(e) =>
-          setNewRecipe({ ...newRecipe, servings: e.target.value })
-        }
-      ></input>
-      <input
-        id={styles.titleInput}
-        placeholder="Prep Time (minutes)"
-        value={newRecipe.prepTime}
-        onChange={(e) =>
-          setNewRecipe({ ...newRecipe, prepTime: e.target.value })
-        }
-      ></input>
-      <input
-        id={styles.titleInput}
-        placeholder="Cook Time (minutes)"
-        value={newRecipe.cookTime}
-        onChange={(e) =>
-          setNewRecipe({ ...newRecipe, cookTime: e.target.value })
-        }
-      ></input>
-      <input
-        id={styles.titleInput}
-        placeholder="Instructions for: (ex. soup, rice, etc)"
-        value={newRecipe.instructions[0]}
-        onChange={(e) =>
-          setNewRecipe({
-            ...newRecipe,
-            instructions: [e.target.value, ''],
-          })
-        }
-      ></input>
-      <input
-        id={styles.titleInput}
-        placeholder="Instructions"
-        value={newRecipe.instructions[1]}
-        onChange={(e) => {
-          let array = [newRecipe.instructions[0], e.target.value]
-          setNewRecipe({
-            ...newRecipe,
-            instructions: array,
-          })
+    <div>
+      <form
+        method="post"
+        className={styles.createRecipe}
+        onSubmit={(e) => {
+          handleSubmit(e)
         }}
-      ></input>
-
-      <input
-        id={styles.photoInput}
-        type="file"
-        name="file"
-        placeholder="Optional: Upload a photo for your recipe"
-        //value={newRecipe.image}
-      ></input>
-      {imageSrc && <img src={imageSrc} className={styles.uploadImage} />}
-      <button
-        type="submit"
-        aria-label="Create Item"
-        className={styles.createSubmit}
-        onClick={() => inputRef.current.focus()}
       >
-        <FaPlus className={styles.FaPlus} />
-      </button>
-    </form>
+        <label htmlFor="createRecipe">Create Recipe</label>
+        <input
+          autoFocus
+          ref={inputRef}
+          id={styles.titleInput}
+          placeholder="Recipe Title"
+          value={newRecipe.title}
+          required
+          onChange={(e) =>
+            setNewRecipe({ ...newRecipe, title: e.target.value })
+          }
+        ></input>
+        <input
+          id={styles.descriptionInput}
+          placeholder="Recipe Description"
+          value={newRecipe.description}
+          required
+          onChange={(e) =>
+            setNewRecipe({ ...newRecipe, description: e.target.value })
+          }
+        ></input>
+        <input
+          id={styles.titleInput}
+          placeholder="Servings"
+          value={newRecipe.servings}
+          onChange={(e) =>
+            setNewRecipe({ ...newRecipe, servings: e.target.value })
+          }
+        ></input>
+        <input
+          id={styles.titleInput}
+          placeholder="Prep Time (minutes)"
+          value={newRecipe.prepTime}
+          onChange={(e) =>
+            setNewRecipe({ ...newRecipe, prepTime: e.target.value })
+          }
+        ></input>
+        <input
+          id={styles.titleInput}
+          placeholder="Cook Time (minutes)"
+          value={newRecipe.cookTime}
+          onChange={(e) =>
+            setNewRecipe({ ...newRecipe, cookTime: e.target.value })
+          }
+        ></input>
+        <input
+          id={styles.titleInput}
+          placeholder="Instructions for: (ex. soup, rice, etc)"
+          value={newRecipe.instructions[0]}
+          onChange={(e) =>
+            setNewRecipe({
+              ...newRecipe,
+              instructions: [e.target.value, ''],
+            })
+          }
+        ></input>
+        <input
+          id={styles.titleInput}
+          placeholder="Instructions"
+          value={newRecipe.instructions[1]}
+          onChange={(e) => {
+            let array = [newRecipe.instructions[0], e.target.value]
+            setNewRecipe({
+              ...newRecipe,
+              instructions: array,
+            })
+          }}
+        ></input>
+
+        <input
+          id={styles.photoInput}
+          type="file"
+          name="file"
+          placeholder="Optional: Upload a photo for your recipe"
+          //value={newRecipe.image}
+        ></input>
+        {imageSrc && <img src={imageSrc} className={styles.uploadImage} />}
+        <button
+          type="submit"
+          aria-label="Create Item"
+          className={styles.createSubmit}
+          onClick={() => inputRef.current.focus()}
+        >
+          <FaPlus className={styles.FaPlus} />
+        </button>
+      </form>
+      <ImageList />
+      <AddImage />
+    </div>
   )
 }
 
