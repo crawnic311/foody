9b7c3a0b162a9166d2b7ad1d7f315eea643a8f92
Dillon Craw <68667382+crawnic311@users.noreply.github.com>
2022-05-19 01:15:19 -0600

    Added navbar and search for case sensitivity

---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,94 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import SearchRecipe from '../Search/SearchRecipe'
+import styles from './navbar.module.css'
+
+const Navbar = ({
+  recipeData,
+  displayRecipe,
+  setDisplayRecipe,
+  recipes,
+  logout,
+}) => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const openMenu = () => {
+    setIsOpen(!isOpen)
+  }
+
+  const handleLogout = () => {
+    console.log('You have logged out.')
+  }
+
+  return (
+    <>
+      <header className={styles.header}>
+        <nav className={styles.navbar}>
+          <Link to="/">
+            <span className={styles.navlogo}>
+              <img
+                src="/foodylogo.png"
+                alt="logo"
+                width="150"
+                height="69"
+                className={styles.foodylogo}
+              />
+            </span>
+          </Link>
+          <span className={(styles.navlink, styles.searchBar)}>
+            <SearchRecipe
+              placeholder={'Search for a recipe... 🔍'}
+              data={recipeData}
+              setDisplayRecipe={setDisplayRecipe}
+              recipes={recipes}
+            />
+          </span>
+          <ul
+            className={
+              isOpen === false
+                ? styles.navmenu
+                : styles.navmenu + ' ' + styles.active
+            }
+          >
+            <li className={styles.navitem}>
+              <Link to="/home">
+                <span className={styles.navlink}>Home</span>
+              </Link>
+            </li>
+            <li className={styles.navitem}>
+              <Link to="/create">
+                <span className={styles.navlink}>Create</span>
+              </Link>
+            </li>
+            <li className={styles.navitem}>
+              <Link to="/about">
+                <span className={styles.navlink}>About</span>
+              </Link>
+            </li>
+            <li className={styles.navitem}>
+              <Link to="/">
+                <button className={styles.navlink} onClick={logout}>
+                  Logout
+                </button>
+              </Link>
+            </li>
+          </ul>
+          <button
+            className={
+              isOpen === false
+                ? styles.hamburger
+                : styles.hamburger + ' ' + styles.active
+            }
+            onClick={openMenu}
+          >
+            <span className={styles.bar}></span>
+            <span className={styles.bar}></span>
+            <span className={styles.bar}></span>
+          </button>
+        </nav>
+      </header>
+    </>
+  )
+}
+
+export default Navbar
---
+++ src/components/search/searchRecipe.js
@@ -0,0 +1,72 @@
+import React, { useState } from 'react'
+import styles from './searchRecipe.module.css'
+
+const SearchRecipe = ({
+  placeholder,
+  data,
+  displayRecipe,
+  setDisplayRecipe,
+  recipes,
+}) => {
+  const [filteredData, setFilteredData] = useState([])
+  const [wordEntered, setWordEntered] = useState()
+
+  const handleFilter = (e) => {
+    const searchWord = e.target.value
+    const newFilter = recipes.filter((value) => {
+      console.log(value.title.toLowerCase().includes(searchWord.toLowerCase()))
+      return value.title.toLowerCase().includes(searchWord.toLowerCase())
+    })
+    if (searchWord === '') {
+      setFilteredData([])
+    } else {
+      setFilteredData(newFilter)
+    }
+  }
+
+  const clearInput = () => {
+    setFilteredData([])
+    setWordEntered('')
+  }
+  return (
+    <div className={styles.SearchRecipes}>
+      <div className={styles.searchWrapper}>
+        <input
+          type="text"
+          value={wordEntered}
+          placeholder={placeholder}
+          className={styles.searchInput}
+          onChange={handleFilter}
+        />
+      </div>
+      {filteredData.length !== 0 && (
+        <div className={styles.searchResult}>
+          {filteredData.slice(0, 10).map((value) => {
+            return (
+              <button
+                className={styles.dataItem}
+                id={value.id}
+                key={value.title}
+                onClick={(e) => {
+                  var targetRecipe = 0
+                  for (let i = 0; recipes.length > i; i++) {
+                    if (recipes[i].id == e.target.id) {
+                      targetRecipe = recipes[i]
+                      break
+                    }
+                  }
+                  setDisplayRecipe(targetRecipe)
+                  clearInput()
+                }}
+              >
+                {value.title}
+              </button>
+            )
+          })}
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default SearchRecipe
